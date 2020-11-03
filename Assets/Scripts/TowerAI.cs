75e587ebd8f4886af9f69d344d1a6200ef171a2b
Kaleb Wasmuth <kalebwasmuth@gmail.com>
2020-11-03 18:54:31 -0500

    Turrets and shooting

---
+++ Assets/Scripts/TowerAI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerAI:MonoBehaviour {
+
+    [SerializeField] Transform towerHead;
+    [SerializeField] Transform enemyObject;
+
+    // Update is called once per frame
+    void Update() {
+        towerHead.LookAt(enemyObject);
+    }
+}
